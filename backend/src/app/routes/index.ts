120a254591e683c9a4df43bc8492d7bee535d0e6
mehedi imun <mehediimun@gmail.com>
2025-08-01 13:10:22 +0600

    project setup with docker

---
+++ backend/src/app/routes/index.ts
@@ -0,0 +1,27 @@
+import { Router } from "express";
+import { AuthRoutes } from "../modules/auth/auth.route";
+
+import { UserRoutes } from "../modules/user/user.route";
+
+export const router = Router();
+
+const moduleRoutes = [
+  {
+    path: "/user",
+    route: UserRoutes,
+  },
+  {
+    path: "/auth",
+    route: AuthRoutes,
+  },
+];
+
+moduleRoutes.forEach((route) => {
+  router.use(route.path, route.route);
+});
+
+// router.use("/user", UserRoutes)
+// router.use("/tour", TourRoutes)
+// router.use("/division", DivisionRoutes)
+// router.use("/booking", BookingRoutes)
+// router.use("/user", UserRoutes)
